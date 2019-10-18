11ab72fbee7efeb61ab782851c4a448a91949267
Zachary_Anderson <zanderson3j@gmail.com>
2019-10-18 00:35:21 -0400

    crazy delay

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -13,10 +13,20 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "SynthSound.h"
 #include "maximilian.h"
+#include "fxobjects.h"
 
 class SynthVoice : public SynthesiserVoice
 {
 public:
+    
+    SynthVoice()
+    {
+        delayBufferL.createCircularBuffer(88200);
+        delayBufferL.flushBuffer();
+        
+        delayBufferR.createCircularBuffer(88200);
+        delayBufferR.flushBuffer();
+    }
     
     bool canPlaySound (SynthesiserSound *sound) override {
         return dynamic_cast<SynthSound*>(sound) != nullptr;
@@ -44,13 +54,42 @@ public:
     
     void renderNextBlock (AudioBuffer<float> &outputBuffer, int startSample, int numSamples) override {
         
+        if (outputBuffer.getNumChannels() != 2) return;
+        
         for (int sample = 0; sample < numSamples; ++sample) {
             
             double sineWave = osc.sinewave(frequency) * level;
             
-            for (int channel = 0; channel < outputBuffer.getNumChannels(); ++channel) {
-                outputBuffer.addSample(channel, startSample + sample, sineWave);
-            }
+            outputBuffer.addSample(0, startSample + sample, sineWave);
+            outputBuffer.addSample(1, startSample + sample, sineWave);
+            
+            float ynL = delayBufferL.readBuffer(24000);
+            float y1L = delayBufferL.readBuffer(12000)*0.5;
+            float y2L = delayBufferL.readBuffer(8000)*0.5;
+            float y3L = delayBufferL.readBuffer(4000)*0.5;
+            float y4L = delayBufferL.readBuffer(2000)*0.5;
+            
+            float ynR = delayBufferR.readBuffer(12000);
+            float y1R = delayBufferR.readBuffer(8000)*0.5;
+            float y2R = delayBufferR.readBuffer(4000)*0.5;
+            float y3R = delayBufferR.readBuffer(2000)*0.5;
+            float y4R = delayBufferR.readBuffer(1000)*0.5;
+            
+            float dnL = outputBuffer.getSample(0, startSample + sample)*0.5 + (0.2*ynL);
+            float dnR = outputBuffer.getSample(1, startSample + sample)*0.5  + (0.5*ynR);
+            
+            delayBufferL.writeBuffer(dnR);
+            delayBufferR.writeBuffer(dnL);
+            
+            outputBuffer.addSample(0,
+                                   startSample + sample,
+                                   outputBuffer.getSample(0, startSample + sample)*0.5
+                                   + (ynL + y1L + y2L + y3L + y4L)*0.5);
+            outputBuffer.addSample(1,
+                                   startSample + sample,
+                                   outputBuffer.getSample(1, startSample + sample)*0.5
+                                   + (ynR + y1R + y2R + y3R + y4R)*0.5);
+            
         }
     }
 
@@ -58,4 +97,6 @@ private:
     double frequency;
     maxiOsc osc;
     float level;
+    CircularBuffer<float> delayBufferL;
+    CircularBuffer<float> delayBufferR;
 };
