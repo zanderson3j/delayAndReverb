7e73d1011fa31c693b3dca9285b3709194385171
Zachary_Anderson <zanderson3j@gmail.com>
2019-10-17 22:43:38 -0400

    get midi input

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -25,6 +25,14 @@ DelayAndReverbAudioProcessor::DelayAndReverbAudioProcessor()
                        )
 #endif
 {
+    synth.clearVoices();
+    
+    for (int i = 0; i < 10; ++i) {
+        synth.addVoice(new SynthVoice());
+    }
+    
+    synth.clearSounds();
+    synth.addSound(new SynthSound);
 }
 
 DelayAndReverbAudioProcessor::~DelayAndReverbAudioProcessor()
@@ -98,6 +106,10 @@ void DelayAndReverbAudioProcessor::prepareToPlay (double sampleRate, int samples
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
+    
+    ignoreUnused(samplesPerBlock);
+    lastSampleRate = sampleRate;
+    synth.setCurrentPlaybackSampleRate(lastSampleRate);
 }
 
 void DelayAndReverbAudioProcessor::releaseResources()
@@ -132,31 +144,34 @@ bool DelayAndReverbAudioProcessor::isBusesLayoutSupported (const BusesLayout& la
 
 void DelayAndReverbAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
 {
-    ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+//    ScopedNoDenormals noDenormals;
+//    auto totalNumInputChannels  = getTotalNumInputChannels();
+//    auto totalNumOutputChannels = getTotalNumOutputChannels();
+//
+//    // In case we have more outputs than inputs, this code clears any output
+//    // channels that didn't contain input data, (because these aren't
+//    // guaranteed to be empty - they may contain garbage).
+//    // This is here to avoid people getting screaming feedback
+//    // when they first compile a plugin, but obviously you don't need to keep
+//    // this code if your algorithm always overwrites all the output channels.
+//    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+//        buffer.clear (i, 0, buffer.getNumSamples());
+//
+//    // This is the place where you'd normally do the guts of your plugin's
+//    // audio processing...
+//    // Make sure to reset the state if your inner loop is processing
+//    // the samples and the outer loop is handling the channels.
+//    // Alternatively, you can process the samples with the channels
+//    // interleaved by keeping the same state.
+//    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+//    {
+//        auto* channelData = buffer.getWritePointer (channel);
+//
+//        // ..do something to the data...
+//    }
+    
+    buffer.clear();
+    synth.renderNextBlock(buffer, midiMessages, 0, buffer.getNumSamples());
 }
 
 //==============================================================================
